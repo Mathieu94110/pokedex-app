00f8303f6306cb0eb0fc811e535726c34edaad03
MathieuEnault <enault.mathieu2@gmail.com>
2021-11-25 20:02:31 +0100

    implementing of infinite scroll with loading and loding message when page getting data

--- pokedex-app/src/app/pages/list-pokemons/list-pokemons.page.ts
+++ pokedex-app/src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { Pokemon } from 'src/app/models/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -9,24 +10,54 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class ListPokemonsPage implements OnInit {
   public pokemons: Pokemon[];
-  constructor(private ps: PokemonService) {
+  constructor(
+    private ps: PokemonService,
+    public loadingCtrl: LoadingController
+  ) {
     this.pokemons = [];
   }
 
   ngOnInit() {
     this.morePokemon();
   }
-  morePokemon() {
+  async morePokemon($event = null) {
     const promise = this.ps.getPokemons();
 
     if (promise) {
+      let loading = null;
+      if (!$event) {
+        loading = await this.loadingCtrl.create({
+          message: 'Veuillez patienter !',
+          duration: 2000,
+        });
+
+        await loading.present();
+      }
+      // const { role, data } = await loading.onDidDismiss();
+      // console.log('Loading dismissed')
+
       promise
-        .then((result: Pokemon[]) => {
+        .then(async (result: Pokemon[]) => {
           this.pokemons = this.pokemons.concat(result);
-          this.pokemons = this.pokemons.sort((p1, p2) => p1.id - p2.id);
+          this.pokemons = this.pokemons.sort((p) => p.id);
           console.log(this.pokemons);
+
+          if ($event) {
+            $event.target.complete();
+          }
+          if (loading) {
+            loading.dismiss();
+          }
         })
-        .catch((error) => console.error(error));
+        .catch((error) => {
+          console.error(error);
+          if ($event) {
+            $event.target.complete();
+          }
+          if (loading) {
+            loading.dismiss();
+          }
+        });
     }
   }
 }
